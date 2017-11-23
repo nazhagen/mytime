17b15cb85276f012e991b5a4f4de2bb020184c9c
Nazeli Hagen <nazelihagen@Nazelis-MacBook-Air.local>
2017-11-23 15:48:34 -0500

    Initial Commit

---
+++ My Time/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  My Time
+//
+//  Created by Nazeli Hagen on 11/23/17.
+//  Copyright © 2017 Nazeli Hagen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
